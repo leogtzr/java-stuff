a0dc6df4c617f29aa6d8258b08de96587bdffbfd
leogtzr <leogutierrezramirez@gmail.com>
2019-08-01 09:53:08 -0600

    Add test project to test ELK

---
+++ elk-demo/src/main/java/com/elk/ElkDemoApplication.java
@@ -0,0 +1,11 @@
+package com.elk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkDemoApplication {
+	public static void main(final String[] args) {
+		SpringApplication.run(ElkDemoApplication.class, args);
+	}
+}
--- elk-demo/src/main/java/com/elk/boundaries/ElkController.java
+++ elk-demo/src/main/java/com/elk/boundaries/ElkController.java
@@ -1,4 +1,49 @@
 package com.elk.boundaries;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
 public class ElkController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ElkController.class);
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/elk")
+    public String helloWorld() {
+        final String response = "Welcome: " + new Date();
+        LOGGER.info("elk endpoint :)");
+        return response;
+    }
+
+    @RequestMapping(value = "/exception")
+    public String exception() {
+        String response = "";
+        try {
+            throw new Exception("Exception has occured....");
+        } catch (final Exception e) {
+            e.printStackTrace();
+            LOGGER.error("alv:", e);
+
+            final StringWriter sw = new StringWriter();
+            final PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            String stackTrace = sw.toString();
+            LOGGER.error("Exception - " + stackTrace);
+            response = stackTrace;
+        }
+        return response;
+    }
+
 }
